1e70e861a9da5fab8c9ed8a046d7b4d1bdb1565f
mucahitcakmak <mucoox18@gmail.com>
2024-05-13 14:57:08 +0300

    Channel file has been changed.

--- Channel/Channel.cpp
+++ Channel/Channel.cpp
@@ -1,1 +1,23 @@
 #include "Channel.hpp"
+
+
+Channel::Channel(std::string name) { this->channelName = name; this->channelAdminSetted = 0;}
+
+std::vector<Client> Channel::getClients() { return this->clients; }
+
+std::string         Channel::getChannelName() { return this->channelName; }
+
+void				Channel::addClient(Client client) { clients.push_back(client); }
+
+int					Channel::getChannelAdminSetted() { return this->channelAdminSetted; }
+
+void				Channel::setAdmin() { this->channelAdminSetted = 1; }
+
+void 				Channel::setClients(std::vector<Client> clients) { this->clients = clients;}
+
+std::string         Channel::getTopic() { return topic; }
+
+void                Channel::setTopic(std::string new_topic) { topic = new_topic; }
+
+
+Channel::~Channel() { }
--- Channel/Channel.hpp
+++ Channel/Channel.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
+#include "../Client/Client.hpp"
+#include <iostream>
+#include <vector>
+
 class Channel
 {
     private:
-		
+        std::vector<Client> clients;
+        std::string channelName;
+		int			channelAdminSetted;
+		std::string topic;
     public:
+        Channel(std::string name);
+        std::vector<Client> getClients();
+        std::string         getChannelName();
+		int					getChannelAdminSetted();
+        // void                setChannelAdmin(Client client);
+		void				setClients(std::vector<Client> clients);
+		void				addClient(Client client);
+		void				setAdmin();
+        void                setTopic(std::string new_topi);
+        std::string         getTopic();
+
+        ~Channel();
 
 };
